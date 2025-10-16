8fb7d46fc92e521c48a751b4a57a01b013a4962a
Rukesh Stha <kuturukesh.55@gmail.com>
2025-10-16 14:10:34 +0545

    views of user is added

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,61 +1,189 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
+from rest_framework.validators import UniqueValidator
+# from django.contrib.auth.password_validation import validate_password
 from .models import User, UserProfile
+
+# class UserProfileSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = UserProfile
+#         fields = ['profile_picture', 'date_of_birth', 'national_id', 'pan_number', 'bio']
+
+
+# class UserRegistrationSerializer(serializers.ModelSerializer):
+#     password = serializers.CharField(write_only = True, validators = [validate_password])
+#     password2 = serializers.CharField(write_only = True)
+#     profile = UserProfileSerializer(required = False)
+
+#     class Meta:
+#         model = User
+#         fields = ('id', 'username', 'email', 'user_type', 'phone', 'address', 'city', 'state', 'pincode', 'first_name', 'last_name', 'is_verified', 'profile', 'password', 'password2')
+#         def validate(self, attrs):
+#             if attrs['password'] != attrs['password2']:
+#                 raise serializers.ValidationError({"password": "password field does not match."})
+#             return attrs
+        
+#         def create(self, validated_date):
+#             profile_data = validated_date.pop('profile', None)
+#             validated_date.pop('password2')
+#             user = User.objects.create_user(**validated_date)
+
+#             if profile_data:
+#                 UserProfile.objects.create(user=user, **profile_data)
+#             else:
+#                 UserProfile.objects.create(user=user)
+#             return user
+
+# # class UserProfileSerializer(serializers.ModelSerializer):
+# #     user = serializers.StringRelatedField(read_only = True)
+
+# #     class Meta:
+# #         model = UserProfile
+# #         fields = ('id', 'username', 'email', 'user_type', 'phone', 'address', 'city', 'state', 'pincode', 'first_name', 'last_name', 'is_verified', 'profile')
+
+# class UserSerializer(serializers.ModelSerializer):
+#     model = User
+#     fields = ('id', 'username', 'email', 'user_type', 'phone', 'address', 'city', 'state', 'pincode', 'first_name', 'last_name', 'is_verified', 'profile', 'date_joined')
+#     read_only_fields = ('id', 'date_joined', 'is_verified')
+
+
+# class ChangePasswordSerializer(serializers.ModelSerializer):
+#     old_password = serializers.CharField(required = True)
+#     new_password = serializers.CharField(required = True, validators = [validate_password])
+
+#     def validate_old_password (self, value):
+#         user = self.context['request'].user
+#         if not user.check_password(value):
+#             raise serializers.ValidationError('Old password is not correct.')
+#         return value
+    
+
+# class LoginSerializer(serializers.Serializer):
+#     username = serializers.CharField()
+#     password = serializers.CharField(write_only=True)
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ['profile_picture', 'date_of_birth', 'national_id', 'pan_number', 'bio']
+        fields = '__all__'
 
 
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only = True, validators = [validate_password])
-    password2 = serializers.CharField(write_only = True)
-    profile = UserProfileSerializer(required = False)
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(read_only = True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'user_type', 'phone', 'address', 'city', 'state', 'pincode', 'first_name', 'last_name', 'is_verified', 'profile', 'password', 'password2')
-        def validate(self, attrs):
-            if attrs['password'] != attrs['password2']:
-                raise serializers.ValidationError({"password": "password field does not match."})
-            return attrs
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'user_type', 'phone_number', 'address', 'city', 'state', 'pincode', 'is_verified', 'profile']
+        read_only_fields = ['id', 'is_verified']
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required = True, validators = [UniqueValidator(queryset = User.objects.all())])     
+    username = serializers.CharField(required = True, validators = [UniqueValidator(queryset = User.objects.all())])     
+    password = serializers.CharField(write_only = True)
+    password2 = serializers.CharField(write_only = True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2', 'user_type', 'phone_number', 'first_name', 'last_name', 'address', 'city', 'state', 'pincode']
+        extra_kwargs = {
+            'first_name': {'required':True},
+            'last_name': {'required':True},
+            'phone_number':{'required': True},
+        }
         
-        def create(self, validated_date):
-            profile_data = validated_date.pop('profile', None)
-            validated_date.pop('password2')
-            user = User.objects.create_user(**validated_date)
 
-            if profile_data:
-                UserProfile.objects.create(user=user, **profile_data)
-            else:
-                UserProfile.objects.create(user=user)
-            return user
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password':'Password fields did not match.'})
+        
+        phone = attrs.get('phone_number', '')
+        if not phone.startswith('+91') and len(phone) == 10:
+            attrs['phone_number'] = f"+91{phone}"
 
-# class UserProfileSerializer(serializers.ModelSerializer):
-#     user = serializers.StringRelatedField(read_only = True)
+        return attrs
 
-#     class Meta:
-#         model = UserProfile
-#         fields = ('id', 'username', 'email', 'user_type', 'phone', 'address', 'city', 'state', 'pincode', 'first_name', 'last_name', 'is_verified', 'profile')
+    
+    def create(self, validated_date):
+        validated_date.pop('password')
+        user = User.objects.create_user(**validated_date)
+        UserProfile.objects.create(user = user)
 
-class UserSerializer(serializers.ModelSerializer):
-    model = User
-    fields = ('id', 'username', 'email', 'user_type', 'phone', 'address', 'city', 'state', 'pincode', 'first_name', 'last_name', 'is_verified', 'profile', 'date_joined')
-    read_only_fields = ('id', 'date_joined', 'is_verified')
+        return user
+        
 
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        if username and password:
+            user = authenticate(username = username, password = password)
+            if not user:
+                try:
+                    user_obj = User.objects.get(email = username)
+                    user = authenticate(username = user_obj.username, password= password)
+                except User.DoesNotExist:
+                    pass
+
+            if not user:
+                raise serializers.ValidationError('unable to login in with the provided credientials.')
+            if not user.is_active:
+                raise serializers.ValidationError('User Account is disabled')
+        else:
+            raise serializers.ValidationError('Must include "username" and "password".')
+        attrs['user'] = user
+        return attrs
 
 class ChangePasswordSerializer(serializers.ModelSerializer):
-    old_password = serializers.CharField(required = True)
-    new_password = serializers.CharField(required = True, validators = [validate_password])
+    old_password = serializers.CharField(required = True, write_only = True)
+    new_password = serializers.CharField(required = True, read_only = True, validators = [validate_password])
+    new_password2 = serializers.CharField(required = True, read_only  = True)
 
-    def validate_old_password (self, value):
-        user = self.context['request'].user
-        if not user.check_password(value):
-            raise serializers.ValidationError('Old password is not correct.')
-        return value
+    def validate (self, attrs):
+        if attrs ['new_password'] != attrs['newpassword2']:
+            raise serializers.ValidationError({'new_password':'Password field didnot match.'})
+        return attrs
     
 
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
+class PasswordResetRequestSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(required = True)
+    uid = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True, write_only=True, validators=[validate_password])
+    new_password2 = serializers.CharField(required=True, write_only=True)
+    
+    def validate(self, attrs):
+        if attrs['new_password'] != attrs['new_password2']:
+            raise serializers.ValidationError({"new_password": "Password fields didn't match."})
+        return attrs
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(required=False)
+    
+    class Meta:
+        model = User
+        fields = [
+            'first_name', 'last_name', 'phone_number', 'address',
+            'city', 'state', 'pincode', 'profile'
+        ]
+    
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile', None)
+        
+        # Update user fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        
+        # Update profile if provided
+        if profile_data:
+            profile = instance.profile
+            for attr, value in profile_data.items():
+                setattr(profile, attr, value)
+            profile.save()
+        
+        return instance
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,82 +1,293 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from .models import User, UserProfile
-from .serializers import UserProfileSerializer, ChangePasswordSerializer, UserRegistrationSerializer, UserSerializer, LoginSerializer
-from django.contrib.auth import authenticate
+from .serializers import UserProfileSerializer, UserRegistrationSerializer, UserSerializer, LoginSerializer, UserUpdateSerializer
+from django.contrib.auth import update_session_auth_hash
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
+# from django.contrib.auth import authenticate
 
-from rest_framework import status, generics, permissions
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken 
+from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = UserRegistrationSerializer
+from equipment.models import Equipment
+# class RegisterView(generics.CreateAPIView):
+#     queryset = User.objects.all()
+#     permission_classes = [permissions.AllowAny]
+#     serializer_class = UserRegistrationSerializer
 
 
-class LoginView(generics.GenericAPIView):
-    serializer_class = LoginSerializer
-    permission_classes = [permissions.AllowAny]
+# class LoginView(generics.GenericAPIView):
+#     serializer_class = LoginSerializer
+#     permission_classes = [permissions.AllowAny]
 
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
+#     def post(self, request):
+#         username = request.data.get('username')
+#         password = request.data.get('password')
         
-        user = authenticate(username=username, password=password)
+#         user = authenticate(username=username, password=password)
 
-        if user:
-            refresh = RefreshToken.for_user(user)
-            user_serializer = UserSerializer(user)
+#         if user:
+#             refresh = RefreshToken.for_user(user)
+#             user_serializer = UserSerializer(user)
 
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-                'user': user_serializer.data
-            })
-        return Response({"error": 'Invalid Credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
+#             return Response({
+#                 'refresh': str(refresh),
+#                 'access': str(refresh.access_token),
+#                 'user': user_serializer.data
+#             })
+#         return Response({"error": 'Invalid Credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
     
 
-class UserProfileView(generics.RetrieveAPIView):
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+# class UserProfileView(generics.RetrieveAPIView):
+#     serializer_class = UserSerializer
+#     permission_classes = [permissions.IsAuthenticated]
 
-    def get_object(self):
-        return self.request.user
+#     def get_object(self):
+#         return self.request.user
 
-class ChangePasswordView(generics.UpdateAPIView):
-    serializer_class = ChangePasswordSerializer
-    permission_classes = [permissions.IsAuthenticated]
+# class ChangePasswordView(generics.UpdateAPIView):
+#     serializer_class = ChangePasswordSerializer
+#     permission_classes = [permissions.IsAuthenticated]
 
-    def get_object(self):
-        return self.request.user
+#     def get_object(self):
+#         return self.request.user
     
-    def update(self, request, *args, **kwargs):
-        user = self.get_object()
-        serializer = self.get_serializer(data = request.data)
+#     def update(self, request, *args, **kwargs):
+#         user = self.get_object()
+#         serializer = self.get_serializer(data = request.data)
+
+#         if serializer.is_valid():
+#             user.set_password(serializer.validated_data["new_password"])
+#             user.save()
+#             return Response({'message': ' Password update successfully'})
+        
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+# class UserProfileDetailView(generics.RetrieveUpdateAPIView):
+#     serializer_class = UserProfileSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+#     def get_object(self):
+#         return self.request.user.profile
+    
+
+# @api_view(['POST'])
+# @permission_classes([permissions.IsAdminUser])
+# def verify_user(request, user_id):
+#     try:
+#         user = User.objects.get(id = user_id)
+#         user.is_verified = True
+#         user.save()
+#         return Response({'message':'User verified successfully'})
+#     except User.DoesNotExist:
+#         return Response({'error':'User not found!'}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    if request.method == 'POST':
+        serializer = UserProfileSerializer(data = request.data)
 
         if serializer.is_valid():
-            user.set_password(serializer.validated_data["new_password"])
-            user.save()
-            return Response({'message': ' Password update successfully'})
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            user_data = UserSerializer(user).data
+
+            # UserProfile.objects.create(user = user)
+            return Response({
+                'message': 'User registered Successfully',
+                'user': user_data,
+                'token':{
+                    'refresh':str(refresh),
+                    'access':str(refresh.access_token),
+                }
+            }, status.HTTP_201_CREATED)
+        return Response(serializer.error, status = status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def user_login(request):
+    if request.method == 'POST':
+        serializer = LoginSerializer(data = request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
+            user_data = UserSerializer(user).data
+
+            return Response({
+                'message': 'Login Successfully',
+                'user': user_data,
+                'token': {
+                    'refresh': str(refresh),
+                    'access':str(refresh.access_token),
+                }
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    try: 
+        refresh_token = request.data.get('refresh')
+        if refresh_token:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        
+        return Response({
+            'message':'Successfully logged out'
+        }, status=status.HTTP_200_OK)
+    
+    except Exception as e:
+        return Response({
+            'error': 'Invalid token',
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def user_logout_all(request):
+    #logout user from all the devices by blacklisting refresh token
+    try:
+        tokens = OutstandingToken.objects.filter(user_id = request.user.id)
+        for token in tokens:
+            BlacklistedToken.objects.get_or_create(token = token)
+
+        return Response({
+            'message': 'Successfully logged out from all deviecs'
+        }, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({
+            'errors':'Could not log out from all devices'
+        },status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def refresh_token(request):
+    #refresh jwt access token
+    try:
+        refresh_token = request.data.get('refresh')
+        if not refresh_token:
+            return Response({
+                'error':'Refresh token is required.'
+
+            }, status=status.HTTP_400_BAD_REQUEST)
+        token = RefreshToken(refresh_token)
+        return Response({
+            'access': str(token.access_token),
+
+        }, status=status.HTTP_200_OK)
+    
+    except Exception as e:
+        return Response({
+            'error':'Invalid or expired refresh token'
+        }, status = status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT', 'GET'])
+@permission_classes([AllowAny])
+def user_profile(request):
+    user = request.user
+    
+    if request.method  == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = UserUpdateSerializer(user, data=request.data, partial = True)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-class UserProfileDetailView(generics.RetrieveUpdateAPIView):
-    serializer_class = UserProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def user_dashboard(request):
+    user = request.user
 
-    def get_object(self):
-        return self.request.user.profile
+    dashboard_data = {
+        'user_info':{
+            'name':user.get_full_name(),
+            'user_type':user.user_type,
+            'email': user.email,
+            'phone':user.phone_number,
+            'is_verified':user.is_verified
+        },
+        'quick_stats':{},
+        'recent_activity':[]
+    }
+
+    if user.user_type == 'customer':
+        # from equipment.models import Equipment
+        from loans.models import LoanApplication
+
+        dashboard_data['quick_stats'] = {
+            'view_equipment':0,
+            'save_items':0,
+            'active_rentals':0,
+            'loan_applications':LoanApplication.objects.filter(appicant = user).count()
+        }
+    elif user.user_type == 'vendor':
+        equipment_stats = Equipment.objects.filter(vendor = user)
+        dashboard_data['quick_stats'] = {
+            'total_equipment': equipment_stats.count(),
+            'verified_equipment': equipment_stats.filter(is_verified= True).count(),
+            'available_equipment': equipment_stats.filter(is_available=True).count(),
+            'pending_inspections':equipment_stats.filter(is_verified=True).count()
+        }
     
+    elif user.user_type == 'franchise':
+        from franchise.models import FranchisePerformance
+        from django.utils import timezone
+        from datetime import datetime
 
-@api_view(['POST'])
-@permission_classes([permissions.IsAdminUser])
-def verify_user(request, user_id):
-    try:
-        user = User.objects.get(id = user_id)
-        user.is_verified = True
-        user.save()
-        return Response({'message':'User verified successfully'})
-    except User.DoesNotExist:
-        return Response({'error':'User not found!'}, status=status.HTTP_404_NOT_FOUND)
+        try: 
+            franchise = user.franchise
+            current_month = timezone.now().replace(day=1)
+            performance = FranchisePerformance.objects.filter(franchise= franchise, month=current_month).first()
+            dashboard_data['quick_stats']={
+                'monthly_sales': performance.total_sales if performance else 0,
+                'active_rentals':performance.total_rentals if performance else 0,
+                'staff_placement':performance.staff_hire_count if performance else 0,
+                'new_customers': performance.customer_count if performance else 0
+            }
+        except:
+            dashboard_data['quick_stats']={
+                'monthly_sales': 0,
+                'active_rentals':0,
+                'staff_placement':0,
+                'new_customers': 0
+            }
+    elif user.user_type == 'staff':
+        from staff.models import StaffAttendance
+        from django.utils import timezone
+
+        today = timezone.now().date()
+        today_attendance = StaffAttendance.objects.filter(staff_user = user, date=today).first()
+        dashboard_data['quick_stats'] = {
+            'today_hours': today_attendance.total_hours if today_attendance else 0,
+            'tasks_completed':0,
+            'pending_task':0,
+            'performance_rating':0     
+        }
+    elif user.user_type == 'admin':
+        from users.models import User
+        from loans.models import LoanApplication
+
+        dashboard_data['quick_stats'] = {
+            'total_users': User.objects.count(),
+            'total_equipment': Equipment.objects.count(),
+            'pending_verifications': Equipment.objects.filter(status = 'submitted').count()
+        }
+    return Response(dashboard_data)
