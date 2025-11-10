30f26a3894e0b9150dfbf0a1a68fb7823fe5db66
Rukesh Stha <kuturukesh.55@gmail.com>
2025-11-10 13:52:21 +0545

    Some Views in vendor is added.

--- backend/vendor/views.py
+++ backend/vendor/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -9,3 +9,51 @@ from .models import Vendor, BusinessDocument
 from .serializers import VendorDocumentSerializer, VendorRegistrationSerializer, VendorSerializer
 
 @api_view(['POST'])
+def vendor_registration(request):
+    if request.method == 'POST':
+        serializer = VendorRegistrationSerializer(data = request.data)
+        if serializer.is_valid():
+            vendor = serializer.save()
+            return Response({
+                'message': 'Vendor registered Successfully.',
+                'vendor_id': vendor.id
+
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET', 'PUT'])
+@permission_classes([IsAuthenticated])
+def vendor_profile(request):
+    try:
+        vendor = Vendor.objects.get(user = request.user)
+    except Vendor.DoesNotExist:
+        return Response(
+            {'error': 'Vendor profile not found.'},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    if request.method == 'GET':
+        serializer = VendorSerializer(vendor)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def vendor_documents(request):
+    vendor = get_object_or_404(Vendor, user = request.user)
+
+    if request.method == 'GET':
+        documents = vendor.documents.all()
+        serializer = VendorDocumentSerializer(documents, many = True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = VendorDocumentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save(vendor = vendor)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
